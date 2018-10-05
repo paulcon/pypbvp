58824eb5c09b48632a9030eeb9950cfc615b8812
Paul Constantine <paul.g.constantine@gmail.com>
2018-10-05 15:44:11 -0600

    commenting code

--- pbvp.py
+++ pbvp.py
@@ -7,10 +7,16 @@ class ParameterizedBoundaryValueProblem():
     '''
     A class for the following parameterized boundary value problem:
     
-    ( kappa(t,x) u'(t,x) )' = 1, u(-1,x) = 0, u'(1,x) = 0, t \in [-1, 1],
+    ( \exp(\kappa(t,x)) u'(t,x) )' = 1, u(-1,x) = 0, u'(1,x) = 0, t \in [-1, 1],
     
     where x represents the vector of parameters that affect the variable
-    coefficient function kappa(t,x). 
+    coefficient function \kappa(t,x). With elementary calculus, we can write the 
+    BVP solution as
+    
+    u(t,x) = \int_{-1}^t (1-t) / \exp(\kappa(t,x)) dt.
+    
+    The class provides a method to estimate the quantity of interest u(1,x) and 
+    its gradient with respect to x.
     
     Attributes
     ----------
@@ -19,10 +25,9 @@ class ParameterizedBoundaryValueProblem():
             
     Methods
     -------
-        
-            
-    Notes
-    -----
+        quantity_of_interest
+            computes the quantity of interest from the BVP solution and its 
+            gradient vector with respect to the parameters
     
     '''
 
@@ -32,23 +37,37 @@ class ParameterizedBoundaryValueProblem():
         '''
         Initialize the boundary value problem with a random field for the 
         variable coefficients.
+
+        Parameters
+        ----------
+            kappa : RandomField
+                initialize the BVP instantiation with a RandomField for the 
+                variable coefficients
         '''
         self.kappa = kappa
                 
     def _integrand(self, Z, n):
         '''
+        Compute the integrand 
         
+        (1-t) / \exp(\kappa(t,x))
+        
+        for all t in a grid on the interval [-1,1] and for x equal to each row
+        of the input matrix Z.
         
         Parameters
         ----------
             Z : ndarray 
-                array of inputs [REWRITE]
+                array of input parameters at which to evaluate the integrand
             n : int
-                number of points [REWRITE]
+                number of points in the uniform discretization of [-1,1]
                 
         Notes
         -----
-    
+        The code uses lazy evaluation of the eigenfunctions associated with the
+        correlation kernel for the variable coefficients kappa at points in the
+        interval [-1,1].
+
         '''
         kappa = self.kappa
         
@@ -62,25 +81,35 @@ class ParameterizedBoundaryValueProblem():
                 
     def quantity_of_interest(self, Z, n=32767, gradflag=False):
         '''
-        
+        Compute the quantity of interest u(1,x) and (optionally) its gradient
+        with respect to the parameters x at several points in the parameter
+        space.
         
         Parameters
         ----------
             Z : ndarray 
-                array of inputs [REWRITE]
-            n : int
-                number of points [REWRITE]
+                array of input parameters at which to evaluate the quantity of 
+                interest and its gradient
+            n : int, optional
+                number of points in the uniform discretization of [-1,1]
+            gradflag : boolean
+                indicates whether or not to compute and return the gradients of
+                the quantity of interest
                 
         Returns
         -------
             f : ndarray
-                stuff [REWRITE]
+                contains evaluations of the quantity of interest at each row
+                of the input Z
             df : ndarray
-                stuff [REWRITE]
-                
+                contains evaluations of the gradient of the quantity of interest
+                with respect to the parameters at each row of the input Z
         Notes
         -----
-    
+        The default discretization parameter n=32767 is in the asymptotic 
+        regime for a wide range of problems defined by different points in the
+        parameter space.
+        
         '''
         kappa = self.kappa
         M = Z.shape[0]
@@ -97,6 +126,7 @@ class ParameterizedBoundaryValueProblem():
         f = np.dot(w, F)
         
         if gradflag:
+            # compute the gradient of the quantity of interest
             B = kappa.eigenvecs * np.sqrt(kappa.eigenvals)
             df = -np.dot(w * F.transpose(), B)
         else:
@@ -106,23 +136,48 @@ class ParameterizedBoundaryValueProblem():
 
 class RandomField():
     """
-    Implementing 1d exponential kernal from Appendix A.2.2 from Fasshauer and 
-    McCourt.
+    Implements the eigenpairs for the exponential correlation kernel
     
-    K(x,z) = exp( - | x - z | / (2*cl) ), where x,z \in [-L, L]
+    K(s,t) = exp( - | s - t | / (2*cl) ), where s,t \in [-L, L]
+    
+    The eigenpairs have explicit forms which can be found in Appendix A.2.2 from
+    Fasshauer and McCourt, Kernel-based Approximation Methods Using MATLAB,
+    World Scientific, NJ, 2015. Once these pairs are calculated for the given
+    variance and correlation length (scale) parameter, then generating 
+    realizations of a Gaussian random field with the same correlation function
+    is straightforward; see the 'evaluate' method.
     
     Attributes
     ----------
-        kappa : RandomField 
-            class that defines the parameterized variable coefficients kappa
+        N : int
+            number of terms in Karhunen-Loeve, must be even
+        eps : float
+            scale parameter for correlation kernel, inversely proportional to 
+            the correlation length
+        L : float
+            domain size [-L,L] for spatial variable
+        w : ndarray
+            intermediate values for computing eigenpairs
+        v : ndarray
+            intermediate values for computing eigenpairs
+        eigenvals : ndarray
+            (N, )-array of Karhunen-Loeve eigenvalues
+        eigenvecs : ndarray
+            (len(t), N)-array of Karhunen-Loeve eigenfunctions evaluated at a 
+            discrete set of points in the spatial domain
+            
             
     Methods
     -------
-        
-            
-    Notes
-    -----
-    
+        compute_eigenvals
+            compute the eigenvalues for the correlation kernel
+        compute_eigenvecs
+            compute the eigenfunctions for the correlation kernel evaluated at
+            a grid in the interval [-1,1]
+        evaluate
+            computes several realizations of a Gaussian random field with the 
+            chosen correlation function
+         
     """
     N = []
     eps = []
@@ -133,8 +188,18 @@ class RandomField():
     
     def __init__(self, N, cl=0.5, sig2=1.):
         '''
-        Initialize the boundary value problem with a random field for the 
-        variable coefficients.
+        Initialize the random field. Compute the intermediate quantities needed
+        to estimate the eigenpairs, and compute the top N eigenvalues.
+
+        Parameters
+        ----------
+            N : int
+                number of terms in the Karhunen-Loeve expansion, must be even
+            cl : float, optional
+                correlation length for the correlation kernel
+            sig2 : float, optional
+                total variance of the Gaussian random field
+
         '''
         
         # let's just make N even
@@ -151,16 +216,20 @@ class RandomField():
         
     def _ev_coeff(self, N):
         '''
-        
+        Compute the intermediate quantities that define the eigenpairs for the 
+        correlation function.
         
         Parameters
         ----------
             N : int
-                number of terms in the KL, must be even [REWRITE]
+                number of terms in the Karhunen-Loeve expansion, must be even
                 
         Notes
         -----
-    
+        This method uses the SciPy root finding method 'brentq' to solve the 
+        root finding problems whose solution defines the intermediate 
+        quantities.
+        
         '''
         # assumes N is even
         w, v = np.zeros((N/2, )), np.zeros((N/2, ))
@@ -191,12 +260,13 @@ class RandomField():
         
     def compute_eigenvals(self):
         '''
-        
-        
+        Compute the first N eigenvalues of the correlation kernel.
                 
         Notes
         -----
-    
+        See Appendix A.2.2 from Fasshauer and McCourt, Kernel-based 
+        Approximation Methods Using MATLAB, World Scientific, NJ, 2015
+
         '''
         eps, sig2, w, v, N = self.eps, self.sig2, self.w, self.v, self.N
         
@@ -207,16 +277,18 @@ class RandomField():
         
     def compute_eigenvecs(self, t):
         '''
-        
+        Compute the eigenfunctions at the grid on the interval [-1,1].
         
         Parameters
         ----------
             t : ndarray
-                points at which to evaluate eigenvecs
-                
+                points in [-1,1] at which to evaluate the eigenfunctions
+
         Notes
         -----
-    
+        See Appendix A.2.2 from Fasshauer and McCourt, Kernel-based 
+        Approximation Methods Using MATLAB, World Scientific, NJ, 2015
+
         '''
         if numgigs(len(t), self.N) > 6:
             raise Warning('Storing the eigenvectors will take more than 6 GB of memory.')
@@ -237,16 +309,20 @@ class RandomField():
         
     def evaluate(self, Z):
         '''
-        
+        Compute several realizations of the Gaussian random field with mean zero
+        and chosen correlation kernel.
         
         Parameters
         ----------
             Z : ndarray 
-                array of inputs [REWRITE]
+                array of points in the parameter space at which to compute 
+                realizations of the random field
                 
         Notes
         -----
-    
+        The points in the parameter space are coefficients of the Karhunen-
+        Loeve expansion associated with the chosen correlation function.
+        
         '''
         N, eigenvals, eigenvecs = self.N, self.eigenvals, self.eigenvecs
         
@@ -256,6 +332,9 @@ class RandomField():
         return np.dot( eigenvecs, np.sqrt(eigenvals).reshape((N, 1))*Z )
         
 def numgigs(nrows, ncols):
+    '''
+    Number of gigabytes of memory needed to store matrix of size nrows-by-ncols.
+    '''
     return nrows*ncols*8.0 / float(2**30)
 
     
